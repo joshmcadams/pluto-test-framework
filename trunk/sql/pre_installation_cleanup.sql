f7aecf557cf83d6d6c3dc4c758684f85475dee60
joshua.mcadams <joshua.mcadams@25896200-c831-0410-a671-29dc5b78e7ec>
2007-06-04 21:20:08 +0000

    cleaned up the installation quite a bit

---
+++ trunk/sql/pre_installation_cleanup.sql
@@ -0,0 +1,24 @@
+set echo on;
+set feedback on;
+set serveroutput on;
+
+declare
+  type varchar_varray is varray( 2 ) of varchar2( 30 );
+
+  va_types  varchar_varray
+                      := varchar_varray( 'PLUTO_OBJ', 'PLUTO_PROC_NAME_TAB' );
+  found_it  number         := 0;
+begin
+  for i in 1 .. va_types.limit
+  loop
+    select count( * )
+      into found_it
+      from user_objects
+     where object_name = va_types( i );
+
+    if found_it > 0 then
+      execute immediate 'drop type ' || va_types( i );
+    end if;
+  end loop;
+end;
+/
