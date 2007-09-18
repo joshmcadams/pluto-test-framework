ebea6ea9cfb8efc49647a1ee17866c4ffa7e17e6
joshua.mcadams <joshua.mcadams@25896200-c831-0410-a671-29dc5b78e7ec>
2007-09-18 21:23:17 +0000

    added self awareness for functional use

--- trunk/sql/utilities/pluto_util_obj.sql
+++ trunk/sql/utilities/pluto_util_obj.sql
@@ -6,13 +6,28 @@ create or replace type pluto_util_obj as object(
   )
     return self as result,
 --
-  member procedure set_test_count( test_count in number ),
+  member procedure set_test_count(test_count in number),
 --
   member procedure finish,
 --
-  member procedure log( message in varchar ),
+  member procedure log(message in varchar),
 --
-  member procedure ok( test_passed in boolean, test_label in varchar ),
+  member function ok(
+    self         in out  pluto_util_obj,
+    test_passed  in      boolean,
+    test_label   in      varchar
+  )
+    return boolean,
+--
+  member procedure ok(test_passed in boolean, test_label in varchar),
+--
+  member function is_ok(
+    self           in out  pluto_util_obj,
+    data_got       in      anydata,
+    data_expected  in      anydata,
+    test_label     in      varchar
+  )
+    return boolean,
 --
   member procedure is_ok(
     data_got       in  anydata,
@@ -20,17 +35,41 @@ create or replace type pluto_util_obj as object(
     test_label     in  varchar
   ),
 --
+  member function is_ok(
+    self           in out  pluto_util_obj,
+    data_got       in      number,
+    data_expected  in      number,
+    test_label     in      varchar
+  )
+    return boolean,
+--
   member procedure is_ok(
     data_got       in  number,
     data_expected  in  number,
     test_label     in  varchar
   ),
 --
+  member function is_ok(
+    self           in out  pluto_util_obj,
+    data_got       in      varchar,
+    data_expected  in      varchar,
+    test_label     in      varchar
+  )
+    return boolean,
+--
   member procedure is_ok(
     data_got       in  varchar,
     data_expected  in  varchar,
     test_label     in  varchar
   ),
+--
+  member function is_ok(
+    self           in out  pluto_util_obj,
+    data_got       in      boolean,
+    data_expected  in      boolean,
+    test_label     in      varchar
+  )
+    return boolean,
 --
   member procedure is_ok(
     data_got       in  boolean,
@@ -38,18 +77,38 @@ create or replace type pluto_util_obj as object(
     test_label     in  varchar
   ),
 --
+  member function is_ok(
+    self           in out  pluto_util_obj,
+    data_got       in      date,
+    data_expected  in      date,
+    test_label     in      varchar
+  )
+    return boolean,
+--
   member procedure is_ok(
     data_got       in  date,
     data_expected  in  date,
     test_label     in  varchar
   ),
 --
-  member procedure is_ok_helper(
-    test_passed    in  boolean,
-    data_got       in  varchar,
-    data_expected  in  varchar,
-    test_label     in  varchar
+  member function is_ok_helper(
+    self           in out  pluto_util_obj,
+    test_passed    in      boolean,
+    data_got       in      varchar,
+    data_expected  in      varchar,
+    test_label     in      varchar
   )
+    return boolean,
+--
+  member function table_exists(
+    self        in out  pluto_util_obj,
+    table_name  in      varchar
+  )
+    return boolean,
+--
+  member procedure table_exists(table_name in varchar)
+
+--
 )
 instantiable not final;
 /
